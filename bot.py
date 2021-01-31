361c15aa15238ea140ac45edc56d848df8ffc16e
Nexus85 <mx38@mail.ru>
2021-01-31 18:10:23 +0300

    начальное состояние

--- bot.py
+++ bot.py
@@ -1,12 +1,20 @@
 import telebot
-from settings import TOKEN
+import os
 
-bot = telebot.TeleBot(TOKEN)
+token = os.environ['TELEGRAM_TOKEN']
+HEROKU = os.environ.get('HEROKU', False)
+
+
+
+bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=['start'])
 def start_cmd(message):
-    bot.reply_to(message, 'Привет!')
+    if HEROKU:
+        bot.reply_to(message, 'Привет! я на Heroku')
+    else:
+        bot.reply_to(message, 'Привет')
 
-
+# https://devcenter.heroku.com/articles/python-runtimes
 
 bot.polling()
